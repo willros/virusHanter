6e1840bffc7e2af851bdac91aca7bf76c0cc1b28
Williams UMU-MacBook <william.rosenbaum@umu.se>
2022-09-02 09:42:04 +0200

    forms

---
+++ forms/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Användarnamn', 
+                           validators=[DataRequired()])
+    password = PasswordField('Lösenord', 
+                             validators=[DataRequired()])
+    submit = SubmitField('Logga in')
+    
